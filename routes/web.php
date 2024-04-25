fc20ec0f5ad2dd5b9bc139843d74c3f4a0e1d8b0
syuja aql <syuja.aqilsky@gmail.com>
2024-04-25 23:07:32 +0700

    Pesan commit Anda di sini

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\authController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::redirect('home', 'dashboard');
+
+Route::get('/auth', [authController::class, "index"])->name('login')->middleware('guest');
+Route::get('/auth/redirect', [authController::class, "redirect"])->middleware('guest');
+Route::get('/auth/callback', [authController::class, "callback"])->middleware('guest');
+Route::get('auth/logout', [authController::class, "logout"]);
+
+Route::get('/dashboard', function(){
+    return "Selamat datang  ".Auth::user()->email." di halaman dashboard";
+})->middleware('auth');
